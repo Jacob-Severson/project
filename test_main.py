8c72e6ee79b6bd3eb75c485133fcd6459f4f73f6
WhiteStride1 <jamesseverson@unomaha.edu>
2022-05-03 10:41:55 -0500

    commit

--- test_main.py
+++ test_main.py
@@ -1,5 +1,10 @@
 import pytest
 from main import *
-
 class test:
     def testnameAndAge(self):
+        self.assertnameAndAge('James', 10) == 'Hello James! You are 20 years old.'
+        self.assertnameAndAge('james', 10) == 'Hello James! You are 20 years old.'
+
+        with self.assertRaises(TypeError):
+            nameAndAge(10, 10)
+            nameAndAge('James', '10')
