7742e9a786c840997bf61f9a14839312b3818e8f
Jacob-Severson <jseverson@unomaha.edu>
2022-05-03 12:48:23 -0500

    changed gui, makes sure you fill all  the inputs

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import gui
 from gui import *
 #TODO Account for invalid data
-#TODO Exeption handle for runtime errors
+#TODO Exception handle for runtime errors
 #TODO test the code with unit test or pytest
 #TODO GUI
 #TODO organize with class or modules
 #TODO store data in files or database
 #TODO ongoing- make sure program is properly documented
 
+
 def nameAndAge(name: str, age: int) -> str:
     """
     function to return the name and age(multiplied by 2)
-    :param name: the name of an indivudal
-    :param age: the age of an indivudial
+    :param name: the name of an individual
+    :param age: the age of an individual
     :return: the name and the age(multiplied by 2)
     """
     with open("namesAndAges.txt","a") as f:
@@ -27,13 +28,13 @@ def main():
     window = Tk()
     window.title("Project 1")
     window.geometry("230x105")
-    window.resizable(False,False)
+    window.resizable(False, False)
 
     widgets = gui.GUI(window)
     window.mainloop()
 
-    name = gui.namegui #FIXME need to get name from gui
-    age = gui.agegui #FIXME need to get age from gui
+    name = namegui #FIXME need to get name from gui
+    age = agegui #FIXME need to get age from gui
     print(nameAndAge(name,age))
 
 
